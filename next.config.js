192e8d7758e16d0775002afa7ec77cc61bc26992
javier0444niama-commits <javier0444niama@gmail.com>
2026-03-11 14:17:34 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   // Si tu repo se llama gym-tracker, cambia esto:
-  // basePath: '/gym-tracker',
-  // assetPrefix: '/gym-tracker/',
+   basePath: '/gym-tracker',
+   assetPrefix: '/gym-tracker/',
   images: {
     unoptimized: true,
   },
